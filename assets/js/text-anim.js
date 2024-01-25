85e27fa3586d85921f3176b0d7fe774d43ce1e90
gascon <gascon@gascon-lenovo-thinkbook-16p-gen.home>
2024-01-25 17:35:56 +0100

    Add hero section and intro

---
+++ assets/js/text-anim.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contactText = document.getElementById("contactText");
+  
+    contactText.addEventListener("mouseenter", function () {
+      const letters = contactText.querySelectorAll(".char");
+  
+      letters.forEach((letter, index) => {
+        letter.style.animation = "animBtnContact 0.5s steps(6) infinite";
+        letter.style.animationDelay = `${index * 0.0833}s`;
+      });
+    });
+  
+    contactText.addEventListener("mouseleave", function () {
+      const letters = contactText.querySelectorAll(".char");
+  
+      letters.forEach((letter) => {
+        letter.style.animation = "none";
+      });
+    });
+  });
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'th-orange': '#EB620C',
+      },
+      fontSize:{
+        'th-h1': '90px',
+        'th-subtittle': '28px',
+      },
+      letterSpacing:{
+        'th-h1': '-4px',
+      },
+      fontFamily:{
+        'th-title': ['Palanquin Dark', 'helvetica', 'arial', 'sans-serif'],
+      },
+      backgroundImage: {
+        'dot': "url('/images/dot.svg')",        
+      },
+      animation:{
+        'dot-line': 'dot-line 30s linear infinite'
+      },
+      keyframes:{
+        'dot-line': {
+          '0%': { transform: 'translateY(-50%)' },
+          '100%': { transform: 'translateY(0)' },          
+        }
+      }
+    },
   },
   plugins: [],
 }
